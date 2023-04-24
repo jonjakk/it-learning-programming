83b8e6244ab5ec534ba548b66d303c0a956ad92e
Jonathan <45264013+jonjakk@users.noreply.github.com>
2023-04-24 14:12:50 +0200

    First Three.js Labyrinth approach

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from evaluate import evaluate
 from translate import translate
 import os
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
+templates = Jinja2Templates(directory="three-maze")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root():
-    return 
+origins = [
+    "http://localhost:9000",
+    "localhost:9000"
+]
+
+# origins = [
+#     "https://it-learning-programming.vercel.app/",
+#     "it-learning-programming.vercel.app"
+# ]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
 
 @app.post("/codecheck")
-async def codecheck(maze: List[int][int], start: str, end: str, code : str):
+async def codecheck(maze: List[List[int]], start: str, end: str, code : str):
     translated_pseudo_code = translate(code, maze)
-    result, score, feedback = evaluate(translated_pseudo_code, maze, start, end)
-    return {"result": result, "score": score, "feedback": feedback}
+    result, score, feedback, path_taken = evaluate(translated_pseudo_code, maze, start, end)
+    return {"result": result, "score": score, "feedback": feedback, "path_taken" : path_taken, "translated_pseudo_code": translated_pseudo_code}
 
 @app.post("/updatePsudoLanguage")
 async def updatePsudoLanguage(word_to_be_updated: str, definition: str):
--- translate.py
+++ translate.py
@@ -1,4 +1,3 @@
-from openai import OpenAI
 import os
 import openai
 
@@ -9,32 +8,5 @@ def translate(code, maze):
     openai.api_key = os.getenv("OPENAI_API_KEY")
     
     #TODO Prime the model with understanding the language
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=primer,
-    temperature=0.7,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-    )
-    #TODO Give it the maze and explain that this is a 
-    response2 = openai.Completion.create(
-    model="text-davinci-003",
-    prompt="Convert this code into python: " + code,
-    temperature=0.7,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-    )
-    response3 = openai.Completion.create(
-    model="text-davinci-003",
-    prompt="Check if correct, else answer with no: " + response2,
-    temperature=0.7,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-    )
-    code = "result = 'Hello World'"
+    
+    #TODO Translate the maze to python which can then be used in exec() in evaluate.py
